f45a6eef9c5c45ef09351b5e4ab765f16b6cff2b
unknown <1415726093@qq.com>
2023-05-15 22:21:44 +0800

    Site updated: 2023-05-15 22:21:43

--- js/runtime.js
+++ js/runtime.js
@@ -1,18 +1,1 @@
-$(function(){
-  var NY = Math.round((new Date('4/17/2019 00:00:00')).getTime()/1000);
-  $('#runtime').flipcountdown({
-    size:"xs",
-    tick:function(){
-      var nol = function(h){
-        return h>9?h:'0'+h;
-      }
-      var	range  	= Math.abs(Math.round((new Date()).getTime()/1000)-NY),
-        secday = 86400, sechour = 3600,
-        days 	= parseInt(range/secday),
-        hours	= parseInt((range%secday)/sechour),
-        min		= parseInt(((range%secday)%sechour)/60),
-        sec		= ((range%secday)%sechour)%60;
-      return nol(days)+' '+nol(hours)+' '+nol(min)+' '+nol(sec);
-    }
-  });
-});
+$((function(){var n=Math.round(new Date("4/17/2019 00:00:00").getTime()/1e3);$("#runtime").flipcountdown({size:"xs",tick:function(){var t=function(n){return n>9?n:"0"+n},e=Math.abs(Math.round((new Date).getTime()/1e3)-n),r=86400,a=3600,i=parseInt(e/r),u=parseInt(e%r/a),o=parseInt(e%r%a/60),s=e%r%a%60;return t(i)+" "+t(u)+" "+t(o)+" "+t(s)}})}));
